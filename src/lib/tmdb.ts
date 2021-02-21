0f028265a5e777abb41fdeb1e99a70ecfc440e40
Lewis Blackburn <lewisblackburn10@gmail.com>
2021-02-21 13:27:49 +0000

    tmdb update

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,8 +1,6 @@
-const TMDB_API_KEY = '50061f964870e6c96229606998cd942c'
-// const TMDB_REFRESH_TOKEN = 'b4831b12ac66d74d6e573d79cd525282a362b75c'
-const TMDB_SESSION = '2420f893f42ce7ba6386e1b23b180d27fc759c5a'
+const {TMDB_API_KEY: key, TMDB_SESSION: session} = process.env
 
-// const getRequestToken = async () => {
+// const getRerfreshToken = async () => {
 //   const response = await fetch(
 //     `https://api.themoviedb.org/3/authentication/token/new?api_key=${TMDB_API_KEY}`,
 //   )
@@ -20,7 +18,7 @@ const TMDB_SESSION = '2420f893f42ce7ba6386e1b23b180d27fc759c5a'
 
 export const getFavouriteMovies = async (page: number) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/account/zxffo/favorite/movies?api_key=${TMDB_API_KEY}&session_id=${TMDB_SESSION}&language=en-US&sort_by=created_at.asc&page=${
+    `https://api.themoviedb.org/3/account/zxffo/favorite/movies?api_key=${key}&session_id=${session}&language=en-US&sort_by=created_at.asc&page=${
       page || 1
     }`,
   )
